17cfe65f1398b2fdb2881f76c5e27ce57b9c829e
gouveiarocha <gouveia.rocha@hotmail.com>
2020-11-26 17:12:44 -0300

    add study codes

--- app/src/main/java/com/example/whatsappclone/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/ConfiguracoesActivity.java
@@ -1,17 +1,28 @@
 package com.example.whatsappclone.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.whatsappclone.R;
 import com.example.whatsappclone.helper.Permissao;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
@@ -19,6 +30,12 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+
+    private ImageView imageViewProfile;
+
+    private ImageButton btnProfileCamera, btnProfileGaleria;
+
+    private static final int SELECAO_CAMERA = 100, SELECAO_GALERIA = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +45,74 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         //Validar permissoes
         Permissao.valiarPermissoes(permissoesNecessarias, this, 1);
 
+        imageViewProfile = findViewById(R.id.image_profile);
+
+        btnProfileCamera = findViewById(R.id.btn_profile_camera);
+        btnProfileGaleria = findViewById(R.id.btn_profile_galeria);
+
         Toolbar toolbar = findViewById(R.id.toolbar_principal);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
 
         //Habilita o botão voltar.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Botão de ação para capturar a foto com a camera.
+        btnProfileCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) { //aula 227 minuto 19.
+                    startActivityForResult(intent, SELECAO_CAMERA);
+                }
+
+            }
+        });
+
+        //Botão de ação para escolher a foto a partir da galeria.
+        btnProfileGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (intent.resolveActivity(getPackageManager()) != null) { //aula 227 minuto 19.
+                    startActivityForResult(intent, SELECAO_GALERIA);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap imagem = null;
+
+            try {
+
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+                if (imagem != null){
+                    imageViewProfile.setImageBitmap(imagem);
+                    Toast.makeText(this, "Imagem alterada com Sucesso!", Toast.LENGTH_SHORT).show();
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
