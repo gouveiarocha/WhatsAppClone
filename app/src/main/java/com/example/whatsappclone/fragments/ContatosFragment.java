735c7db6adfdb37a443528b7b391f776f3658450
gouveiarocha <gouveia.rocha@hotmail.com>
2020-12-02 15:20:24 -0300

    add study codes

--- app/src/main/java/com/example/whatsappclone/fragments/ContatosFragment.java
+++ app/src/main/java/com/example/whatsappclone/fragments/ContatosFragment.java
@@ -3,18 +3,34 @@ package com.example.whatsappclone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.whatsappclone.R;
+import com.example.whatsappclone.adapter.ContatosAdapter;
+import com.example.whatsappclone.helper.FirebaseUtils;
+import com.example.whatsappclone.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
+
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
+    private ValueEventListener valueEventListener;
 
 
     public ContatosFragment() {
@@ -26,7 +42,54 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //Config iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+
+        //Config adapter
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        //Config recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        FirebaseUtils.refUsuarios().removeEventListener(valueEventListener);
+        listaContatos.clear();
+    }
+
+    public void recuperarContatos() {
+        valueEventListener = FirebaseUtils.refUsuarios().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    listaContatos.add(usuario);
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
